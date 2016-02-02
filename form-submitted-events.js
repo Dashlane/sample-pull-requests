d46249cf62071afe5f62d22e9209e1b9fc35ef02
Denis Sokolov <denis.sokolov@dashlane.com>
2016-02-02 11:00:09 +0200

    Translated the submit function

--- form-submitted-events.js
+++ form-submitted-events.js
@@ -1,13 +1,23 @@
+var languages = window.languages;
 var popups = window.AwesomePopups;
 
 window.contactUsFormSubmitted = function(isSuccess) {
   if (isSuccess) {
+    var text = 'Thank you for contacting us!'
+    if (languages.enabled() && languages.current === 'fr') {
+      // text = 'Merci de nous écrire!'
+      text = 'Merci de nous avoir contacter!'
+    }
     popups.show({
       icon: 'icons/checkmark.png',
-      text: 'Thank you for contacting us!'
+      text: text
     });
   } else {
-    popups.showError('Sadly, we could not send the email.');
+    if (languages.enabled() && languages.current === 'fr') {
+      popups.showError('Pardon. Impossible d’envoyer email.');
+    } else {
+      popups.showError('Sadly, we could not send the email.');
+    }
   }
 };
 
