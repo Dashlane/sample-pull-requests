b7d390cbdbbacafd449f4892b202f63b7d2861bd
Denis Sokolov <denis.sokolov@dashlane.com>
2016-02-02 11:06:58 +0200

    Translate contact form submit popups into French

--- form-submitted-events.js
+++ form-submitted-events.js
@@ -1,13 +1,22 @@
+var languages = window.languages;
 var popups = window.AwesomePopups;
 
 window.contactUsFormSubmitted = function(isSuccess) {
+  var successText = 'Thank you for contacting us!';
+  var errorText = 'Sadly, we could not send the email.';
+
+  if (languages.enabled() && languages.current === 'fr') {
+    successText = 'Merci de nous avoir contacter!';
+    errorText = 'Pardon. Impossible d’envoyer email.';
+  }
+
   if (isSuccess) {
     popups.show({
       icon: 'icons/checkmark.png',
-      text: 'Thank you for contacting us!'
+      text: successText
     });
   } else {
-    popups.showError('Sadly, we could not send the email.');
+    popups.showError(errorText);
   }
 };
 
